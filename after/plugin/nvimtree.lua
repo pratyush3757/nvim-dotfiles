4dcd3b7e823a3d936fa7ad5d2a524d3e96400811
Pro3757 <pratyush3757@gmail.com>
2024-07-02 16:12:07 +0530

    nvim-tree show all gitignore files by default

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -6,4 +6,10 @@ vim.g.loaded_netrwPlugin = 1
 vim.keymap.set("n", "<leader>t", vim.cmd.NvimTreeToggle)
 
 -- empty setup using defaults
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    git = {
+        enable = true,
+        ignore = false,
+        timeout = 400,
+    },
+})
